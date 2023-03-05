454d1c7715007b10600704f93f366d2aef1edfb8
artas728 <andrew.volotscov@gmail.com>
2023-03-04 18:24:27 -0800

    add schema draft

---
+++ app/schema.py
@@ -0,0 +1,28 @@
+import typing
+import strawberry
+from app.models import HistoryWeatherType
+from app.resolvers import get_weather, get_all_favourites, save_favorite
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    async def weather(city: str, date: str) -> HistoryWeatherType:
+        return await get_weather(city=city, date=date)
+
+    @strawberry.field
+    async def favorites() -> typing.List[HistoryWeatherType]:
+        return await get_all_favourites()
+
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    async def create_favorite(
+            self,
+            city: str,
+            date: str,
+            temperature: float,
+            humidity: float
+    ) -> HistoryWeatherType:
+        return await save_favorite(city, date, temperature, humidity)
